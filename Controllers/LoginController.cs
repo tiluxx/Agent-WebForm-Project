cd8137ac3c5b71979275500a0218f154e4849a79
JLBiLong <trantobaolong0802@gmail.com>
2023-04-30 23:41:28 +0700

    Add Login function & Fix some issues.

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Agent_WebForm_Prodject.Models;
+using Agent_WebForm_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Agent_WebForm_Project.Controllers
+namespace Agent_WebForm_Prodject.Controllers
 {
     public class LoginController : Controller
     {
@@ -12,6 +14,32 @@ namespace Agent_WebForm_Project.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(UserAccount userAccountModel)
+        {
+            using (DistributorDBEntities db = new DistributorDBEntities())
+            {
+                var agentAccounts = db.UserAccounts.Where(staff => staff.UserName == userAccountModel.UserName && staff.UserPassword == userAccountModel.UserPassword).FirstOrDefault();
+                if (agentAccounts == null)
+                {
+                    userAccountModel.LoginMessageError = "Invalid account";
+                    return View("Index", userAccountModel);
+                }
+                else
+                {
+                    StaffAccount staffAccount = new agentAccounts();
+                    Session["StaffID"] = staffAccount.GetStaffID(userAccountModel.UserName);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
